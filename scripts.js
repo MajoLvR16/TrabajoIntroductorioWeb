c3efd4a10d289bc932f727fb8ed61b22916fc10e
MajoLvR16 <mariajoselr@ufps.edu.co>
2024-08-26 11:44:13 -0500

    Añadi la logica de registro en scripts.js

--- scripts.js
+++ scripts.js
@@ -2,15 +2,28 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     var loginForm = document.getElementById("loginForm");
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
 
-        if (username === "admin" && password === "1234") {
-            window.location.href = "dashboard.html";
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
-    });
+            // Verificación de usuario y contraseña
+            if (username === "admin" && password === "1234") {
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
+    var registroForm = document.getElementById("registroForm");
+    if (registroForm) {
+        registroForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var newUser = document.getElementById("newUser").value;
+            var newPassword = document.getElementById("newPassword").value;
+            alert("Usuario registrado: " + newUser);
+            window.location.href = "index.html";
+        });
+    }
 });
